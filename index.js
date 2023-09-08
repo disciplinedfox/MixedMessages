7d3574160e7948f2bb7106a46c01f9c3ffabacac
Andrew Bloom <105402149+disciplinedfox@users.noreply.github.com>
2023-09-08 11:37:20 -0700

    Create daily activities object with categorical activities as arrays

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function generateRandomNumber(num) {
     // Gets # from 0 -> num - 1
     return Math.floor(Math.random() * num)
   }
+
+const dailyActivities = {
+    health: ["going for a jog", "doing yoga", "lifting weights", "using a new healthy recipe to cook"],
+    mind: ["reading a book", "watching a personal development video", "practicing a new language", "playing music"],
+    relationships: ["meeting a friend for coffee", "calling a loved one who is long distance", "buying a gift for someone special", "volunteering for the community"]
+}
