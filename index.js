e73f091449df3d8cf1ad8f7a453a0012f8dca755
Andrew Bloom <105402149+disciplinedfox@users.noreply.github.com>
2023-09-08 11:49:42 -0700

    Add object itereated for activities

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ const dailyActivities = {
     mind: ["reading a book", "watching a personal development video", "practicing a new language", "playing music"],
     relationships: ["meeting a friend for coffee", "calling a loved one who is long distance", "buying a gift for someone special", "volunteering for the community"]
 }
+
+let todaysActivity = [];
+//store selected categorical activity in an array
+
+// Iterate over the object
+for(let prop in dailyActivities) {
+    let optionIdx = generateRandomNumber(dailyActivities[prop].length);
+    switch(prop) {
+        case 'health':
+          todaysActivity.push(`Your sign right now is a "${dailyActivities[prop][optionIdx]}".`)
+          break
+        case 'mind':
+          todaysActivity.push(`You are having: "${dailyActivities[prop][optionIdx]}".`)
+          break
+        case 'relationships':
+          todaysActivity.push(`You should: "${dailyActivities[prop][optionIdx]}".`)
+          break
+        default:
+          todaysActivity.push('There is not enough info.')
+      }
+}
